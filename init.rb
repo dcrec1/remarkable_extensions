21ab19e3ce88e2efd2c77748d9dd19ffff36555c
Diego Carrion <dc.rec1@gmail.com>
2009-06-23 11:53:38 -0300

    fix bug that prevented the plugin working with metric_fu

--- init.rb
+++ init.rb
@@ -1,9 +1,8 @@
 if RAILS_ENV == "test"
 
+  require 'spec'
+  require 'spec/rails'
   require 'remarkable_rails'
 
   require File.join(File.dirname(__FILE__), "lib", "remarkable_extensions")
-
-  require 'spec'
-  require 'spec/rails'
 end
