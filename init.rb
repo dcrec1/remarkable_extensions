26b7235d51af69685b5728433662b21c5ae68258
Vinicius B. Fuentes <vinibaggio@gmail.com>
2009-08-24 02:06:25 +0800

    Adding locales to Remarkable load path automatically (solution by Willian Molinari)
    Signed-off-by: Diego Carrion <dc.rec1@gmail.com>

--- init.rb
+++ init.rb
@@ -4,5 +4,8 @@ if RAILS_ENV == "test"
   require 'spec/rails'
   require 'remarkable_rails'
 
+Remarkable.add_locale RAILS_ROOT + "/vendor/plugins/remarkable_extensions/locales/en.yml"
+Remarkable.add_locale RAILS_ROOT + "/vendor/plugins/remarkable_extensions/locales/pt.yml"
+
   require File.join(File.dirname(__FILE__), "lib", "remarkable_extensions")
 end
